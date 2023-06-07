f3f88edfbecb8849770fd2b55f0676c0c4692dce
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-06-07 09:27:47 +0200

    CommentForm and ComentContainer

--- src/components/Comment/CommentForm.tsx
+++ src/components/Comment/CommentForm.tsx
@@ -8,29 +8,57 @@ import { RootState } from '@/store';
 import Error from '../Error/Error';
 import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useRef, useState } from 'react';
+import Textarea from '../UI/Textarea';
 interface Props {
   post_id: number;
 }
 export default function CommentForm({ post_id }: Props) {
+  const mode = useSelector((state: RootState) => state.mode);
+
+  useEffect(() => {
+    if (formik.values.comment === '') {
+      if (inputRef.current) {
+        inputRef.current.innerText = 'What do you think?';
+        inputRef.current.style.color = '#ccc';
+      }
+    }
+  }, []);
   const router = useRouter();
   const currentUser = useSelector((state: RootState) => state.user);
+  const [loading, setLoading] = useState<boolean>(false);
+  const inputRef = useRef<HTMLDivElement>(null);
   const handleCommentSent = async (
     content: string,
     user_id: string,
     post_id: string
   ) => {
+    setLoading(true);
+
     try {
       const response = await axios.post('/post/comment', {
         content: content,
         user_id: user_id,
         post_id: post_id,
       });
-      console.log(response.data);
-      toast.success('Successfully sent');
+
+      if (response) {
+        toast.success('Successfully sent');
+        formik.values.comment = '';
+        if (inputRef.current) inputRef.current.innerText = '';
+      }
+      router.refresh();
     } catch (e: any) {
       toast.error(e.response.message);
+      router.refresh();
+    } finally {
+      setLoading(false);
+      router.refresh();
     }
   };
+
   const formik = useFormik({
     initialValues: {
       comment: '',
@@ -47,28 +75,71 @@ export default function CommentForm({ post_id }: Props) {
       return errors;
     },
     onSubmit: (values) => {
-      router.refresh();
       handleCommentSent(values.comment, currentUser.id, post_id.toString()!);
     },
   });
   return (
-    <div>
+    <div className='text-md '>
       <form
+        className='flex h-full justify-center px-6 gap-3 dark:text-white   items-center'
         onSubmit={formik.handleSubmit}
-        className='flex p-4 gap-2 justify-center items-center '
       >
-        <FormInput
+        <div
+          ref={inputRef}
+          className='w-full pl-2 rounded-md flex items-center text-black  text-sm min-h-[2.5rem] border-brown border-2'
+          style={{
+            overflow: 'auto',
+            whiteSpace: 'pre-wrap',
+            wordWrap: 'break-word',
+          }}
+          onFocus={() => {
+            if (inputRef.current && formik.values.comment === '') {
+              inputRef.current.innerText = '';
+            }
+          }}
           id='comment'
-          name='comment'
-          onChange={formik.handleChange}
-          value={formik.values.comment}
-          placeholder='What do you think?'
+          contentEditable={true}
+          onInput={(event: React.KeyboardEvent<HTMLDivElement>) => {
+            const content = event.currentTarget.textContent;
+            if (inputRef.current)
+              inputRef.current.style.color =
+                mode.mode === 'dark' ? '#d1d5db' : 'black';
+
+            formik.setFieldValue('comment', content);
+          }}
+          onPaste={(event: React.ClipboardEvent<HTMLDivElement>) => {
+            event.preventDefault();
+            const text = event.clipboardData.getData('text/plain');
+            document.execCommand('insertText', false, text);
+          }}
+          onBlur={(event: React.FocusEvent<HTMLDivElement>) => {
+            formik.handleBlur(event);
+            if (inputRef.current && formik.values.comment === '') {
+              inputRef.current.style.color = '#ccc';
+              inputRef.current.innerText = 'What do you think';
+            }
+          }}
+          onKeyDown={(event: React.KeyboardEvent<HTMLDivElement>) => {
+            if (event.key === 'Enter') {
+              event.preventDefault();
+              formik.handleSubmit();
+            }
+          }}
+        />
+
+        <Button
           variant={'default'}
-        ></FormInput>
-        <Button type='submit'>Send</Button>
+          className=''
+          isLoading={loading}
+          onClick={() => {
+            formik.submitForm();
+          }}
+        >
+          <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
+        </Button>
       </form>
       {formik.touched.comment && formik.errors.comment ? (
-        <Error>{formik.errors.comment}</Error>
+        <Error className='px-6'>{formik.errors.comment}</Error>
       ) : null}
     </div>
   );
--- src/components/commentContainer.tsx
+++ src/components/commentContainer.tsx
@@ -3,21 +3,38 @@ import { Comment } from '@/lib/types/types';
 interface Props {
   comment: Comment;
 }
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from './UI/Button';
+import CommentSettingsModal from './UI/Modals/CommentSettningsModal';
+import { useModal } from '@nextui-org/react';
+import Paragraph from './UI/Paragraph';
+import { formatDate } from '@/util/getDate';
+import Link from 'next/link';
 
 const CommentContainer: FC<Props> = ({ comment }) => {
   return (
-    <div className=' px-6 py-1 '>
-      <div className='p-4 bg-gray-200 rounded-lg'>
-        <div className='flex  gap-3'>
-          <img
-            className='w-6 h-6 rounded-full   '
-            src={comment.user.image || comment.user.imageUri}
-            alt=''
-          />
-          <div>{comment.user.username}</div>
+    <div className=' px-6 text-sm '>
+      <div className='p-4  dark:bg-brown  bg-gray-50  rounded-lg'>
+        <div className='flex relative  gap-3'>
+          <Link href={`/${comment.user.username}`} className='flex gap-3'>
+            <img
+              className='w-6 h-6 rounded-full   '
+              src={comment.user.image || comment.user.imageUri}
+              alt=''
+            />
+            <span>{comment.user.username}</span>
+          </Link>
+          <Paragraph className='absolute right-6'>
+            {formatDate(comment.created_at?.toString())}
+          </Paragraph>
+
+          <CommentSettingsModal></CommentSettingsModal>
         </div>
 
-        <p className='px-3 py-3'>{comment.content}</p>
+        <p style={{ wordBreak: 'break-word' }} className='break-words	 px-12'>
+          {comment.content}
+        </p>
       </div>
     </div>
   );
