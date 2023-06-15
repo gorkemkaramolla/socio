bc224a19fb3140a162b82b7ddcc8c5b7b4c2776a
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-06-15 13:43:02 +0200

    redirect added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -91,5 +91,8 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
+    async redirect({ url, baseUrl }: { url: any; baseUrl: any }) {
+      return baseUrl;
+    },
   },
 };
