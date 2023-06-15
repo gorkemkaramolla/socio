bf937e6daf5e06d27c5d7538aebf3233af6a988b
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-06-15 13:39:27 +0200

    RootPage for / root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
-'use client';
-import React from 'react';
-import HelloText from '@/components/HelloText';
-import Heading from '@/components/UI/Heading';
-import RegisterPage from '@/components/Login/Register';
+import React, { useState } from 'react';
+
+import { Metadata } from 'next';
+import RootPage from '@/components/Root/RootPage';
+
+// either Static metadata
+export const metadata: Metadata = {
+  title: 'Socio',
+  description:
+    'Socio: Stay connected, share your thoughts, and discuss on the ultimate social media platform for developers. Connect with like-minded individuals, explore trending topics, and stay up-to-date with the latest in the developer community. Join Socio today and experience a new level of social interaction for developers.',
+  keywords:
+    'Socio, Stay connected, Share your thoughts, Discuss, Social Media platform, Developers, Developer community, Social interaction, Trending topics, Like-minded individuals, Latest updates, Connect, Explore',
+};
 
 export default function Home() {
-  return (
-    <div className='w-screen p-4 gap-4 relative h-screen flex justify-center items-center'>
-      <div className='w-1/2 flex justify-center items-center'>
-        <Heading heading='h1' weight={'extra'} size={'xl'}>
-          Always Stay connected
-        </Heading>
-      </div>
-      <div className='w-1/2  p-4 flex justify-center items-center'>
-        {/* <Heading heading='h1' weight={'extra'} size={'xl'}>
-          Stay connected
-        </Heading> */}
-        <div className='dark:bg-blackSwan border-2  w-full h-[90vh] border-blackSwan'>
-          <RegisterPage />
-        </div>
-      </div>
-    </div>
-  );
+  return <RootPage />;
 }
