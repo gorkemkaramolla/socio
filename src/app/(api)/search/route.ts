f038ea5e160d3a93f671f5e90844e123b46b27d1
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-06-18 09:49:07 +0200

    SSE implemented

--- src/app/(api)/search/route.ts
+++ src/app/(api)/search/route.ts
@@ -1,36 +1,65 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
+import { resizeImage } from '@/util/imageSharp';
+
 export async function GET(req: Request) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const username = searchParams.get('username');
-    if (username) {
+  const { searchParams } = new URL(req.url);
+  const username = searchParams.get('username');
+
+  if (username) {
+    try {
       await prisma.$connect();
-      const user = await prisma.user.findMany({
+      const users = await prisma.user.findMany({
         where: {
-          username: {
-            contains: username,
-          },
+          OR: [
+            { username: { contains: username } },
+            { name: { contains: username } },
+          ],
+        },
+        select: {
+          name: true,
+          email: true,
+          image: true,
+          id: true,
+          bio: true,
+          imageUri: true,
+          username: true,
+          location: true,
         },
       });
+      // users.map(async (user) => {
+      //   const resizedImageBuffer = await resizeImage(user.image!, 48);
+      //   user.image = resizedImageBuffer;
+      // });
+
       prisma.$disconnect();
-      if (user) {
-        return new NextResponse(JSON.stringify(user), {
+
+      if (users.length > 0) {
+        const data = JSON.stringify(users);
+        const headers = {
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          Connection: 'keep-alive',
+        };
+
+        const body = `data: ${data}\n\n`;
+
+        return new NextResponse(body, {
           status: 200,
-          headers: { 'Content-Type': 'application/json' },
+          headers,
         });
       } else {
-        return new NextResponse('no found', {
+        return new NextResponse('No users found', {
           status: 404,
           headers: { 'Content-Type': 'application/json' },
         });
       }
+    } catch (e: any) {
+      console.error(e);
+      return new NextResponse(e, {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      });
     }
-  } catch (e: any) {
-    console.error(e);
-    return new NextResponse(e, {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
   }
 }
