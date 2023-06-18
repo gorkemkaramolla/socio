7c7da523be76971d62b4681146d1ee3db427b08c
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-06-18 09:52:27 +0200

    prisma  server only and serialized

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,28 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient({});
+
+// globalForPrisma.prisma = prisma;
 import { PrismaClient } from '@prisma/client';
+import 'server-only';
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var cachedPrisma: PrismaClient;
+}
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({});
+let prismax: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prismax = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prismax = global.cachedPrisma;
+}
 
-globalForPrisma.prisma = prisma;
+export const prisma = prismax;
